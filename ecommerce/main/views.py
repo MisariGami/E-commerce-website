0b3197537a311911475460ceb97d48f4df6c0293
MisariGami <20ce029@charusat.edu.in>
2022-12-31 19:23:02 +0530

    create category page

--- ecommerce/main/views.py
+++ ecommerce/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Home page
 def home(request):
     return render(request, 'index.html')
+
+# Category
+def category_list(request):
+    return render(request, 'category_list.html')
