03045c43919779ee46f4180223dd3228744948d6
yashvi <20ce045@charusat.edu.in>
2023-01-05 00:02:11 +0530

    product_list

--- ecommerce/main/views.py
+++ ecommerce/main/views.py
@@ -20,8 +20,8 @@ def product_list(request):
     data=Product.objects.all().order_by('-id')
     cats=Product.objects.distinct().values('category__title','category__id')
     brands=Product.objects.distinct().values('brand__title','brand__id')
-    colors=ProductAttribute.objects.distinct().values('color__title','color__id','color__color_code')
-    sizes=ProductAttribute.objects.distinct().values('size__title','size__id')
+    colors=Product.objects.distinct().values('color__title','color__id','color__color_code')
+    sizes=Product.objects.distinct().values('size__title','size__id')
     return render(request, 'product_list.html', 
     {
         'data':data,
